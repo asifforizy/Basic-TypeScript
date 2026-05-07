717f059c30578ebb4e68ef7171857734699acc7c
MD Saifur Rahaman <asifforizy@gmail.com>
2026-05-07 14:44:08 +0600

    problem 3 solved

--- solutions.ts
+++ solutions.ts
@@ -1,7 +1,7 @@
 // ! problem 1 
 
 function filterEvenNumbers(numbers: number[]): number[] {
-  return numbers.filter(num => num % 2 === 0);
+    return numbers.filter(num => num % 2 === 0);
 }
 // const result = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 // console.log(result);
@@ -9,9 +9,24 @@ function filterEvenNumbers(numbers: number[]): number[] {
 // ! problem 2 
 
 const reverseString = (input: string): string => {
-  let reversed = "";
-  for (let i = input.length - 1; i >= 0; i--) {
-    reversed += input[i];
-  }
-  return reversed;
+    let reversed = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        reversed += input[i];
+    }
+    return reversed;
 };
+
+
+// ! problem 3 
+
+type StringOrNumber = string | number;
+
+const checkType = (value: StringOrNumber): string => {
+    if (typeof value === "string") {
+        return "String";
+    } else {
+        return "Number";
+    }
+};
+
+
