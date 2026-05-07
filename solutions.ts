a87399a8f3566f5ede943e8cc0272e3ed35349b3
MD Saifur Rahaman <asifforizy@gmail.com>
2026-05-07 14:47:24 +0600

    problem 4 solved

--- solutions.ts
+++ solutions.ts
@@ -29,4 +29,9 @@ const checkType = (value: StringOrNumber): string => {
     }
 };
 
+// ! problem 4
+
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+    return obj[key];
+};
 
